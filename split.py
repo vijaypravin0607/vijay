dc5f603e35cf8b889d3d8b1c0482e2b26e64fce3
vijay <vijaypravin0609@gmail.com>
2023-09-01 14:40:34 +0530

    new

---
+++ split.py
@@ -0,0 +1,8 @@
+"""import requests
+x=requests.get("https://www.w3schools.com/")
+print(x.text)
+import requests
+x=requests.post("https://www.w3schools.com/")
+print(x.text)"""
+import calendar
+print(calendar.month(0,1))
